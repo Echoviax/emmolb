ca9608ccb35d739b2082a3b84b3550f23f17ece1
Corey Shaw <github.oxidant041@simplelogin.com>
2025-08-03 14:10:50 -0500

    Persisted cache with IndexedDB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,21 +8,36 @@ import { SettingsProvider } from "@/components/Settings";
 import { ThemeUpdater } from "@/components/ThemeUpdater";
 import { Navbar } from "@/components/Navbar";
 import { usePathname } from "next/navigation";
+import { QueryClient } from "@tanstack/react-query";
+import { createIDBPersister } from "@/lib/persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
-export default function RootLayout({ children, }: Readonly<{children: React.ReactNode;}>) {
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            gcTime: 1000 * 60 * 60 * 24, // 24 hours
+        },
+    },
+})
+
+const persister = createIDBPersister()
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     const pathname = usePathname();
     const hideNavbar = pathname.includes('live');
-  
+
     return (
-        <html lang="en">
-            <body className={`${GeistSans.className} ${GeistMono.variable} min-h-screen`}>
-                {!hideNavbar && <Navbar />}                
-                <SettingsProvider>
-                    <ThemeUpdater />
-                    {children}
-                </SettingsProvider>
-                <Analytics />
-            </body>
-        </html>
+        <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }} >
+            <html lang="en">
+                <body className={`${GeistSans.className} ${GeistMono.variable} min-h-screen`}>
+                    {!hideNavbar && <Navbar />}
+                    <SettingsProvider>
+                        <ThemeUpdater />
+                        {children}
+                    </SettingsProvider>
+                    <Analytics />
+                </body>
+            </html>
+        </PersistQueryClientProvider>
     );
 }
