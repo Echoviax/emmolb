6a34ace836ffd53bf460d3fd640a7cd1abd60941
Corey Shaw <github.oxidant041@simplelogin.com>
2025-08-02 23:33:58 -0500

    Add limit param to live endpoint

--- src/app/nextapi/game/[id]/live/route.ts
+++ src/app/nextapi/game/[id]/live/route.ts
@@ -3,6 +3,9 @@ import { NextRequest } from 'next/server';
 export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const after = req.nextUrl.searchParams.get('after') ?? '0';
+  const limit = Number(req.nextUrl.searchParams.get('limit')) ?? undefined;
+  if (limit && limit < 0)
+    return new Response(null, {status: 400, statusText: 'limit must be positive'});
 
   const response = await fetch(`https://mmolb.com/api/game/${id}/live?after=${after}`, {
     headers: { 'Accept': 'application/json' },
@@ -12,21 +15,13 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ id: 
     return new Response(null, { status: response.status, statusText: response.statusText });
   }
 
-  const data = await response.json();
-
-  // Check if last event indicates game over
-  const entries = data.entries ?? [];
-
-  if (entries.length === 0){
-    return new Response(null, { status: 500, statusText: 'No new events!!'})
-  }
-
-  const lastEvent = entries.length > 0 ? entries[entries.length - 1] : null;
-
-  // Example condition - adjust to your actual data structure
-  if (lastEvent?.event === "Recordkeeping") {
-    return new Response(null, { status: 410, statusText: 'Game Over' });
-  }
+  let data = await response.json();
+  if (limit) {
+    data = {
+      ...data,
+      entries: data.entries?.slice(-limit) ?? [],
+    }
+  };
 
   // Otherwise return the data as usual
   return new Response(JSON.stringify(data), {
