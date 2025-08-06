c7dfdeaa21f8273e7f5d61b204fce04b85d675dd
Corey Shaw <github.oxidant041@simplelogin.com>
2025-08-05 21:52:42 -0500

    Lesser league standings queries

--- src/hooks/api/League.tsx
+++ src/hooks/api/League.tsx
@@ -98,7 +98,7 @@ export function getLeagueTopTeamsQueryOptions<TData = Team[]>({ leagueId, ...opt
     };
 }
 
-export function useLeagueTopTeams<TData>(options: LeagueTopTeamsQueryOptions<TData>) {
+export function useLeagueTopTeams<TData = Team[]>(options: LeagueTopTeamsQueryOptions<TData>) {
     return useQuery(getLeagueTopTeamsQueryOptions(options));
 }
 
