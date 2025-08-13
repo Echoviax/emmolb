6170c3868678bafa4fa1c4846abc5553406236a0
Corey Shaw <github.oxidant041@simplelogin.com>
2025-08-13 17:26:02 -0500

    Rewrote parsing logic to handle more cases

--- src/components/team/TeamFeed.tsx
+++ src/components/team/TeamFeed.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Loading from "@/components/Loading";
 import Link from "next/link";
-import { useDeferredValue, useEffect, useMemo, useState } from "react";
+import { ReactNode, useDeferredValue, useEffect, useMemo, useState } from "react";
 import CheckboxDropdown from "../CheckboxDropdown";
 import { useTeamFeed } from "@/hooks/api/Team";
 import { Team } from "@/types/Team";
@@ -87,44 +87,45 @@ export function TeamFeed({ team }: TeamFeedProps) {
                     <input type='text' className='text-sm bg-(--theme-primary) p-1 rounded-sm' onChange={evt => setSearchText(evt.target.value)} />
                 </div>
             </div>
-            <div className={`grid gap-x-2 md:gap-x-4 mt-4 max-w-4xl ${selectedSeason !== 'all' ? 'grid-cols-[max-content_max-content_auto]' : 'grid-cols-[max-content_max-content_max-content_auto]'}`}>
+            <div className={`grid gap-x-2 md:gap-x-4 my-4 max-w-4xl ${selectedSeason !== 'all' ? 'grid-cols-[max-content_max-content_auto]' : 'grid-cols-[max-content_max-content_max-content_auto]'}`}>
                 {selectedSeason === 'all' && <div className='row-1 col-2 py-1 text-center text-xs uppercase font-semibold'>Season</div>}
                 <div className={`row-1 ${selectedSeason !== 'all' ? 'col-2' : 'col-3'} py-1 text-center text-xs uppercase font-semibold`}>Day</div>
                 {filteredFeed.map((event, i) => {
-                    const parts = event.text.split(/( vs\. | - )/);
                     let day = event.day;
                     if (day === 'Superstar Break')
                         day = 'SSB';
                     else if (day === 'Holiday')
                         day = 'Hol';
 
+                    let lastIndex = 0;
+                    const nodes: ReactNode[] = [];
+                    for (const link of event.links) {
+                        if (link.match === `${team.emoji} ${team.location} ${team.name}`)
+                            continue;
+
+                        const startIndex = event.text.indexOf(link.match, lastIndex);
+                        if (startIndex === -1)
+                            continue;
+
+                        if (startIndex > lastIndex)
+                            nodes.push(event.text.substring(lastIndex, startIndex));
+
+                        const href = link.type === 'game' ? `/watch/${link.id}` : `/${link.type}/${link.id}`;
+                        lastIndex = startIndex + link.match.length;
+                        nodes.push(<Link className='text-theme-link underline cursor-pointer' href={href}>
+                            {event.text.substring(startIndex, lastIndex)}
+                        </Link>);
+                    }
+                    if (lastIndex < event.text.length)
+                        nodes.push(event.text.substring(lastIndex));
+
                     return (
-                        <div className='col-span-full grid-cols-subgrid grid items-baseline p-1 text-sm odd:bg-(--theme-primary) even:bg-(--theme-secondary) border-t-1 border-(--theme-text)/50' key={i}>
+                        <div className='col-span-full grid-cols-subgrid grid items-baseline p-1 text-sm odd:bg-(--theme-primary) even:bg-(--theme-secondary) border-t-1 last:border-b-1 border-(--theme-text)/50' key={i}>
                             <div className='col-auto text-lg pl-2'>{event.emoji}</div>
                             {selectedSeason === 'all' && <div className='col-auto text-center text-base font-semibold'>{event.season}</div>}
                             <div className='col-auto text-center text-base font-semibold'>{day}</div>
                             <div className='col-auto pr-2 pb-0.5'>
-                                {event.type === 'game' ? (() => {
-                                    let linkIndex = 0;
-                                    return parts.map((part: string, index: number) => {
-                                        if (/^\s*vs\.\s*$|^\s*-\s*$/.test(part)) { return <span key={`sep-${index}`}>{part}</span>; }
-
-                                        const link = event.links?.[linkIndex];
-                                        linkIndex++;
-
-                                        if (!link) {
-                                            return <span key={`text-${index}`}>{part}</span>;
-                                        }
-
-                                        const href = link.type === 'game' ? `/watch/${link.id}` : `/${link.type}/${link.id}`;
-
-                                        return (
-                                            <Link key={`link-${index}`} className='text-theme-link' href={href}>
-                                                <span className="underline cursor-pointer">{part}</span>
-                                            </Link>
-                                        );
-                                    });
-                                })() : event.text}
+                                {nodes}
                             </div>
                         </div>
                     );                    
