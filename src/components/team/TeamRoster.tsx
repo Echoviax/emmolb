11b55bcd1ef877730a61f7d95d8feb8f9f6df94d
Corey Shaw <github.oxidant041@simplelogin.com>
2025-08-12 20:04:34 -0500

    Extract roster to a separate component

---
+++ src/components/team/TeamRoster.tsx
@@ -0,0 +1,200 @@
+'use client'
+import Loading from "@/components/Loading";
+import { useEffect, useState } from "react";
+import { Team, TeamPlayer } from "@/types/Team";
+import { DerivedPlayerStats } from "@/types/PlayerStats";
+import { Player } from "@/types/Player";
+import ExpandedPlayerStats from "../ExpandedPlayerStats";
+import { usePlayers } from "@/hooks/api/Player";
+
+const statKeyMap: Record<string, string> = {
+    "AVG": "ba",
+    "OBP": "obp",
+    "SLG": "slg",
+    "OPS": "ops",
+    "Hits": "hits",
+    "Singles": "singles",
+    "Doubles": "doubles",
+    "Triples": "triples",
+    "Home Runs": "home_runs",
+    "Total Bases": "total_bases",
+    "Walked": "walked",
+    "Hit By Pitch": "hit_by_pitch",
+    "Struck Out": "struck_out",
+    "Plate Appearances": "plate_appearances",
+    "At Bats": "at_bats",
+    "Stolen Bases": "stolen_bases",
+    "Caught Stealing": "caught_stealing",
+    "Grounded Into Double Plays": "grounded_into_double_play",
+    "ERA": "era",
+    "WHIP": "whip",
+    "K/BB": "kbb",
+    "K/9": "k9",
+    "H/9": "h9",
+    "BB/9": "bb9",
+    "HR/9": "hr9",
+    "Innings Pitched": "ip",
+    "Strikeouts": "strikeouts",
+    "Walks": "walks",
+    "Hits Allowed": "hits_allowed",
+    "Hit Batters": "hit_batters",
+    "Earned Runs": "earned_runs",
+    "Wins": "wins",
+    "Losses": "losses",
+    "Quality Starts": "quality_starts",
+    "Saves": "saves",
+    "Blown Saves": "blown_saves",
+    "Appearances": "appearances",
+    "Games Finished": "games_finished",
+    "Complete Games": "complete_games",
+    "Shutouts": "shutouts",
+    "No Hitters": "no_hitters",
+    "Errors": "errors",
+    "Assists": "assists",
+    "Putouts": "putouts",
+    "Double Plays": "double_plays",
+    "Runners Caught Stealing": "runners_caught_stealing"
+};
+
+
+type TeamRosterProps = {
+    team: Team;
+}
+
+export function TeamRoster({ team }: TeamRosterProps) {
+    const { data: players, isPending: playersIsPending } = usePlayers({
+        playerIds: team?.players?.map(p => p.player_id)
+    });
+
+    const [sortStat, setSortStat] = useState<string>('');
+    const [expandedPlayers, setExpandedPlayers] = useState<Record<string, boolean>>({});
+    useEffect(() => {
+        if (team?.players)
+            setExpandedPlayers(Object.fromEntries(team.players.map((player: TeamPlayer) => [player.player_id, false])));
+    }, [team?.players]);
+
+    if (playersIsPending) return (
+        <>
+            <Loading />
+        </>
+    );
+
+    const reverseSortStats = new Set([
+        "era", "whip", "bb9", "h9", "hr9", "losses", "blown_saves", "errors"
+    ]);
+
+    const sortedPlayers = team.players ? [...team.players].sort((a, b) => {
+        if (!sortStat || !players) return 0;
+
+        const statKey = statKeyMap[sortStat] as keyof DerivedPlayerStats;
+        const aStats = a.stats[statKey];
+        const bStats = b.stats[statKey];
+
+        const aValue = typeof aStats === 'number' ? aStats : Infinity;
+        const bValue = typeof bStats === 'number' ? bStats : Infinity;
+
+        const reverse = reverseSortStats.has(statKey);
+
+        if (!Number.isFinite(aValue) && !Number.isFinite(bValue)) return 0;
+        if (!Number.isFinite(aValue)) return 1;
+        if (!Number.isFinite(bValue)) return -1;
+
+        return reverse ? aValue - bValue : bValue - aValue;
+    }) : [];
+
+    return (
+        <>
+            <div className="mb-4 text-center">
+                <label className="mr-2 font-semibold">Sort by:</label>
+                <select className="bg-theme-primary text-theme-text px-2 py-1 rounded" value={sortStat} onChange={(e) => setSortStat(e.target.value)}>
+                    <option value="">Default</option>
+                    <optgroup label="Batting">
+                        <option value="AVG">AVG</option>
+                        <option value="OBP">OBP</option>
+                        <option value="SLG">SLG</option>
+                        <option value="OPS">OPS</option>
+                        <option value="Hits">Hits</option>
+                        <option value="Singles">Singles</option>
+                        <option value="Doubles">Doubles</option>
+                        <option value="Triples">Triples</option>
+                        <option value="Home Runs">Home Runs</option>
+                        <option value="Total Bases">Total Bases</option>
+                        <option value="Walked">Walked</option>
+                        <option value="Hit By Pitch">Hit By Pitch</option>
+                        <option value="Struck Out">Struck Out</option>
+                        <option value="Plate Appearances">Plate Appearances</option>
+                        <option value="At Bats">At Bats</option>
+                        <option value="Stolen Bases">Stolen Bases</option>
+                        <option value="Caught Stealing">Caught Stealing</option>
+                        <option value="Grounded Into Double Plays">Grounded Into Double Plays</option>
+                    </optgroup>
+                    <optgroup label="Pitching">
+                        <option value="ERA">ERA</option>
+                        <option value="WHIP">WHIP</option>
+                        <option value="K/BB">K/BB</option>
+                        <option value="K/9">K/9</option>
+                        <option value="H/9">H/9</option>
+                        <option value="BB/9">BB/9</option>
+                        <option value="HR/9">HR/9</option>
+                        <option value="Innings Pitched">Innings Pitched</option>
+                        <option value="Strikeouts">Strikeouts</option>
+                        <option value="Walks">Walks</option>
+                        <option value="Hits Allowed">Hits Allowed</option>
+                        <option value="Hit Batters">Hit Batters</option>
+                        <option value="Earned Runs">Earned Runs</option>
+                        <option value="Wins">Wins</option>
+                        <option value="Losses">Losses</option>
+                        <option value="Quality Starts">Quality Starts</option>
+                        <option value="Saves">Saves</option>
+                        <option value="Blown Saves">Blown Saves</option>
+                        <option value="Appearances">Appearances</option>
+                        <option value="Games Finished">Games Finished</option>
+                        <option value="Complete Games">Complete Games</option>
+                        <option value="Shutouts">Shutouts</option>
+                        <option value="No Hitters">No Hitters</option>
+                    </optgroup>
+                    <optgroup label="Defense">
+                        <option value="Errors">Errors</option>
+                        <option value="Assists">Assists</option>
+                        <option value="Putouts">Putouts</option>
+                        <option value="Double Plays">Double Plays</option>
+                        <option value="Runners Caught Stealing">Runners Caught Stealing</option>
+                        <option value="RCS%">RCS%</option>
+                    </optgroup>
+                </select>
+            </div>
+            <div className="flex justify-center">
+                <div className="w-128 space-y-2">
+                    {sortedPlayers.map((player, i) => {
+                        const statKey = statKeyMap[sortStat] as keyof DerivedPlayerStats;
+                        const rawStat = statKey && player ? player.stats[statKey] : null;
+                        const formattedStat = typeof rawStat === 'number' ? !Number.isFinite(rawStat) ? '-' : ['ba', 'obp', 'slg', 'ops', 'era', 'whip', 'kbb', 'k9', 'bb9', 'h9', 'hr9'].includes(statKey!) ? rawStat.toFixed(3) : Math.round(rawStat).toString() : '';
+                        return (
+                            <div key={i}>
+                                <div className="flex justify-between items-center p-1 rounded link-hover cursor-pointer transition" onClick={() => { setExpandedPlayers(prev => ({ ...prev, [player.player_id]: !prev[player.player_id], })) }}>
+                                    <div className="flex items-center gap-3 w-full">
+                                        <span className="w-4 text-xl text-center">{player.emoji}</span>
+                                        <span className="w-8 text-sm text-right">#{player.number}</span>
+                                        <span className="w-6 text-sm font-bold text-theme-text opacity-80 text-right">{player.position}</span>
+                                        <span className="flex-1 font-semibold text-left overflow-hidden text-ellipsis whitespace-nowrap">{player.first_name} {player.last_name}</span>
+                                        {sortStat && (
+                                            <span className="ml-auto w-20 text-right text-sm opacity-70 text-theme-text font-mono">
+                                                {formattedStat}
+                                            </span>
+                                        )}
+                                    </div>
+                                </div>
+                                {expandedPlayers[player.player_id] && (() => {
+                                    const statsPlayer = players?.find(p => p?.id === player.player_id);
+                                    if (!statsPlayer) return null;
+
+                                    return <ExpandedPlayerStats player={{ ...(player as any), ...(statsPlayer as Player), }} />;
+                                })()}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
+}
