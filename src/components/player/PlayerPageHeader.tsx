15d4bd689b10eef61686106cc21d7c62b81c5020
Corey Shaw <github.oxidant041@simplelogin.com>
2025-10-10 16:12:27 -0500

    Fix item emoji and text overflow

--- src/components/player/PlayerPageHeader.tsx
+++ src/components/player/PlayerPageHeader.tsx
@@ -7,32 +7,31 @@ import { getContrastTextColor } from "@/helpers/ColorHelper";
 
 export type EquipmentTooltipProps = {
     equipment: Equipment | undefined;
-    emoji: string;
     name: string;
     isActive: boolean;
     onToggle: () => void;
     appendName?: boolean; // Hey this is just so the optimize page isn't funky
 };
 
-export function EquipmentTooltip({ equipment, emoji, name, isActive, onToggle, appendName = true }: EquipmentTooltipProps) {
+export function EquipmentTooltip({ equipment, name, isActive, onToggle, appendName = true }: EquipmentTooltipProps) {
     const itemBorder: Record<string, string> = { 'Normal': '#1c2a3a', 'Magic': '#42A5F5', 'Rare': '#FFEE58' };
     const itemFont: Record<string, string> = { 'Normal': 'text-gray-500', 'Magic': 'text-blue-500', 'Rare': 'text-yellow-500' };
     const itemColor: Record<string, string> = { 'Normal': 'from-gray-500 to-gray-700', 'Magic': 'from-blue-500 to-blue-700', 'Rare': 'from-yellow-500 to-yellow-700' };
     const formattedName = equipment ? `${equipment.prefix?.join(' ') ?? ''} ${equipment.name ?? ''} ${equipment.suffix?.join(' ') ?? ''}`.trim() : name;
     return (
         <div className="relative group" onClick={(e) => { e.stopPropagation(); onToggle(); }}>
-            <div className="w-18 h-18 border-3 text-theme-primary rounded-lg flex flex-col items-center justify-center shadow cursor-pointer" style={{ borderColor: itemBorder[equipment?.rarity ?? 'Normal'] }}>
+            <div className="w-18 min-h-18 border-3 text-theme-primary rounded-lg flex flex-col items-center justify-center shadow cursor-pointer" style={{ borderColor: itemBorder[equipment?.rarity ?? 'Normal'] }}>
                 <div className="text-3xl">
-                    {equipment ? emoji : '❔'}
+                    {equipment ? equipment.emoji : '❔'}
                 </div>
                 <div className="text-xs font-semibold text-center mt-1 px-1" style={{ fontSize: 8 }}>{equipment?.rareName ?? formattedName}{appendName ? equipment?.rareName ? ` ${equipment.name}` : '' : ''}</div>
                 <div className={`absolute bottom-[-2rem] left-1/2 -translate-x-1/2 transition-opacity duration-200 pointer-events-none group-hover:opacity-100 z-40 ${isActive ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
                     <div className="pointer-events-none z-[100] ">
                         <div className="bg-theme-primary border-2 border-theme-accent rounded-xl shadow-xl text-center text-xs w-56 overflow-hidden">
                             <div className={`bg-gradient-to-r ${itemColor[equipment ? equipment.rarity : 'Normal']} text-white relative font-bold py-1 px-2 flex items-center justify-center`}>
-                                <span className="absolute left-1 top-1/2 -translate-y-1/2">{emoji}</span>
+                                <span className="absolute left-1 top-1/2 -translate-y-1/2">{equipment?.emoji}</span>
                                 <span className="mx-4 whitespace-normal break-words text-center">{equipment?.rareName ?? formattedName}{appendName ? equipment?.rareName ? ` ${equipment.name}` : '' : ''}</span>
-                                <span className="absolute right-1 top-1/2 -translate-y-1/2">{emoji}</span>
+                                <span className="absolute right-1 top-1/2 -translate-y-1/2">{equipment?.emoji}</span>
                             </div>
                             <div className="p-2 space-y-1">
                                 <div className="text-[10px]">
@@ -74,7 +73,7 @@ function BoonTooltip({ boon, type, isActive, onToggle }: BoonTooltipProps) {
 
     return (
         <div className="relative group" onClick={(e) => { e.stopPropagation(); onToggle(); }}>
-            <div className={`w-18 h-18 border-3 ${borderColor} text-theme-primary rounded-lg flex flex-col items-center justify-center shadow cursor-pointer`}>
+            <div className={`w-18 min-h-18 border-3 ${borderColor} text-theme-primary rounded-lg flex flex-col items-center justify-center shadow cursor-pointer`}>
                 <div className="text-3xl">
                     {boon.emoji}
                 </div>
@@ -133,11 +132,11 @@ export function PlayerPageHeader({ player, team }: PlayerPageHeaderProps) {
             )}
 
             <div className="flex justify-center flex-wrap gap-2">
-                <EquipmentTooltip equipment={player.equipment.head} emoji='🧢' name='Head' isActive={activeTooltip === 'head'} onToggle={() => toggle('head')} />
-                <EquipmentTooltip equipment={player.equipment.body} emoji='👕' name='Body' isActive={activeTooltip === 'body'} onToggle={() => toggle('body')} />
-                <EquipmentTooltip equipment={player.equipment.hands} emoji='🧤' name='Hands' isActive={activeTooltip === 'hands'} onToggle={() => toggle('hands')} />
-                <EquipmentTooltip equipment={player.equipment.feet} emoji='👟' name='Feet' isActive={activeTooltip === 'feet'} onToggle={() => toggle('feet')} />
-                <EquipmentTooltip equipment={player.equipment.accessory} emoji='💍' name='Accessory' isActive={activeTooltip === 'accessory'} onToggle={() => toggle('accessory')} />
+                <EquipmentTooltip equipment={player.equipment.head} name='Head' isActive={activeTooltip === 'head'} onToggle={() => toggle('head')} />
+                <EquipmentTooltip equipment={player.equipment.body} name='Body' isActive={activeTooltip === 'body'} onToggle={() => toggle('body')} />
+                <EquipmentTooltip equipment={player.equipment.hands} name='Hands' isActive={activeTooltip === 'hands'} onToggle={() => toggle('hands')} />
+                <EquipmentTooltip equipment={player.equipment.feet} name='Feet' isActive={activeTooltip === 'feet'} onToggle={() => toggle('feet')} />
+                <EquipmentTooltip equipment={player.equipment.accessory} name='Accessory' isActive={activeTooltip === 'accessory'} onToggle={() => toggle('accessory')} />
             </div>
 
             <div className="grid grid-rows-2 grid-flow-col gap-3 max-w-xl w-full mx-auto mb-4">
